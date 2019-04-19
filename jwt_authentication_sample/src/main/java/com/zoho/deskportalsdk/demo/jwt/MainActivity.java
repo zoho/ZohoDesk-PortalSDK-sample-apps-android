345cb8e3637e252933f4349817d4131f8aab4ce6
vignesh-zt132 <vignesh.thillai@zohocorp.com>
2019-04-19 12:18:22 +0530

    setusertoken is migrated check added

--- jwt_authentication_sample/src/main/java/com/zoho/deskportalsdk/demo/jwt/MainActivity.java
+++ jwt_authentication_sample/src/main/java/com/zoho/deskportalsdk/demo/jwt/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zoho.deskportalsdk.demo.jwt;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,22 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void setUserToken(View view) {
         EditText userTokenView = (EditText) findViewById(R.id.user_token);
-        progrssBar.setVisibility(View.VISIBLE);
-        MyApplication.zohoDeskPortalSDKInstnace.setUserToken(userTokenView.getText().toString(), new DeskCallback.DeskSetUserCallback() {
-            @Override
-            public void onUserSetSuccess() {
-                Toast.makeText(getApplicationContext(), "User set success", Toast.LENGTH_SHORT).show();
-                progrssBar.setVisibility(View.GONE);
-                MyApplication.zohoDeskPortalSDKInstnace.startDeskHomeScreen(MainActivity.this);
-            }
+        if(!MyApplication.zohoDeskPortalSDKInstnace.isUserSignedIn() || !isMigratedTo1_0()) {
+            progrssBar.setVisibility(View.VISIBLE);
+            MyApplication.zohoDeskPortalSDKInstnace.setUserToken(userTokenView.getText().toString(), new DeskCallback.DeskSetUserCallback() {
+                @Override
+                public void onUserSetSuccess() {
+                    Toast.makeText(getApplicationContext(), "User set success", Toast.LENGTH_SHORT).show();
+                    progrssBar.setVisibility(View.GONE);
+                    MyApplication.zohoDeskPortalSDKInstnace.startDeskHomeScreen(MainActivity.this);
+                    isMigratedTo1_0(true);
+                }
 
-            @Override
-            public void onException(DeskException e) {
-                Toast.makeText(getApplicationContext(), "User set failure", Toast.LENGTH_SHORT).show();
-                progrssBar.setVisibility(View.GONE);
-            }
-        });
-
+                @Override
+                public void onException(DeskException e) {
+                    Toast.makeText(getApplicationContext(), "User set failure", Toast.LENGTH_SHORT).show();
+                    progrssBar.setVisibility(View.GONE);
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, "User Token already set.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void logoutFromSDK(View view) {
@@ -50,5 +57,17 @@ public class MainActivity extends AppCompatActivity {
             public void onException(DeskException exception) {
             }
         });
+    }
+
+    private boolean isMigratedTo1_0() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        return settings.getBoolean("isMigrated", false);
+    }
+
+    private void isMigratedTo1_0(boolean isMigrated) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("isMigrated", isMigrated);
+        editor.commit();
     }
 }
